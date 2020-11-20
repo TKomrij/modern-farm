785eaee257b14128279242a8f441805038b456fd
TKomrij <tatiane.b.komrij@gmail.com>
2020-11-20 11:28:05 -0800

    add tractor.js and populate plants array with yearlyPlan array

--- src/scripts/tractor.js
+++ src/scripts/tractor.js
@@ -1,15 +1,25 @@
+import {addPlant} from './field.js'
+import {createAsparagus} from './seeds/asparagus.js'
+import {createCorn} from './seeds/corn.js'
+import {createPotato} from './seeds/potato.js'
+import {createSoybean} from './seeds/soybean.js'
+import {createSunflower} from './seeds/sunflower.js'
+import {createWheat} from './seeds/wheat.js'
 
-// const yearPlan =
-// [
-//   ["Potato", "Soybean", "Soybean", "Corn"],
-//   ["Wheat", "Corn", "Wheat", "Asparagus"],
-//   ["Asparagus", "Wheat", "Soybean", "Corn"],
-//   ["Asparagus", "Soybean", "Potato", "Wheat"]
-// ]
-
-
-
-// export const plantSeeds = (plantingPlan) => {
-
-
-// }
+export const plantSeeds = (plan) => {
+  for (let i = 0; i < plan.length; i++) {
+    for (let j = 0; j < plan[i].length; j++) {
+      if (plan[i][j] === "Potato") {
+        addPlant(createPotato())
+      } else if (plan[i][j] === "Asparagus") {
+        addPlant(createAsparagus())
+      } else if (plan[i][j] === "Potato") {
+        addPlant(createCorn())
+      } else if (plan[i][j] === "Soybean") {
+        addPlant(createSoybean())
+      } else if (plan[i][j] === "Wheat") {
+        addPlant(createWheat())
+      } else (addPlant(createSunflower()))
+    }
+  }
+}
